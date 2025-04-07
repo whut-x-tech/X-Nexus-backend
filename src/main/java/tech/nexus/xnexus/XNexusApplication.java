51e9f170235bcace4b79ec53cf227a3714c5f8b9
lucifer_miao_丁浩然 <133748832+9lucifer@users.noreply.github.com>
2025-04-07 21:31:29 +0800

    feat(init):初始化项目

---
+++ src/main/java/tech/nexus/xnexus/XNexusApplication.java
@@ -0,0 +1,13 @@
+package tech.nexus.xnexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XNexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tech/nexus/xnexus/XNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.nexus.xnexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XNexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
